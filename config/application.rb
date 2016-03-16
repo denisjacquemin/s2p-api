77c33fd11109c0c59083a89d3a13918e393afe87
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-03-16 22:02:06 +0100

    Moving Cors config from dev to all envs

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,12 @@ module S2pAPI
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*' # TODO add restrictions for Cors requests
+        resource '*', :headers => :any, :methods => [:get]
+      end
+    end
   end
 end
