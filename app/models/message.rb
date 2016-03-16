a8565c81ce265da10f3af0c2a3ff5ba322a8a744
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-03-16 22:01:41 +0100

    adding since scope to Message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ApplicationRecord
 
   scope :by_group_ids, ->(group_ids) { where("groups && ARRAY[?]::integer[]", group_ids) }
+  scope :since, ->(date) { where("updated_at > ?", date) }
 end
