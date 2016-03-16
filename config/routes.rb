65de9aec3796927543f3ff3f5bc641c1527d9847
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-03-16 22:02:28 +0100

    removing latest service and adding route for messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  post 'latest', to: 'messages#latest'
+  get 'messages', to: 'api#messages'
   get 'code_label/:code', to: 'api#code_label'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
