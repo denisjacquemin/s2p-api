3aac2f7b46c1f84746d7f3cd5aff97c88f6f03aa
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-03-16 22:00:56 +0100

    adding messages service

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,8 +1,25 @@
 class ApiController < ApplicationController
+
+  # /messages?last_update=2016-03-16&codes[]=123456&codes[]=7KGWpG"
+  def messages
+    # receive codes corresponding to a student
+    #codes = params[:codes].map{|code| {code: code[:code], latest_update: code[:latest_update]}}
+    codes = params[:codes]
+
+    # find the group based on the given codes
+    students = Student.by_codes(codes)
+    groups = students.map{ |s| s.groups }.flatten
+
+    last_update = params[:last_update]
+    # find messages based on the groups found
+    @messages = Message.by_group_ids(groups).since(last_update)
+    render json: @messages
+  end
+
   def code_label
     code_label = Student.by_codes(params[:code])
     message = 'Code erroné'
-    if code_label.first
+    if code_label.first N7ljVx
       message = code_label.first.fullname
     end
     render json: message
