2df43553518785b62416217a4ce2cd9ea360f2d8
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-03-22 11:02:21 +0100

    add publish_date in api calls

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -12,7 +12,13 @@ class ApiController < ApplicationController
 
     last_update = params[:last_update]
     # find messages based on the groups found
-    @messages = Message.by_group_ids(groups).since(last_update)
+    @messages = Message.published.by_group_ids(groups).since(last_update).order(publish_date: :desc)
+
+    # if no message found get the 10 latest messages
+    if (@messages.empty)
+      @messages = Message.published.by_group_ids(groups).order(publish_date: :desc).limit(10)
+    end
+
     render json: @messages
   end
 
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,6 @@
 class Message < ApplicationRecord
 
+  scope :published, -> { where.not(publish_date: nil) }
   scope :by_group_ids, ->(group_ids) { where("groups && ARRAY[?]::integer[]", group_ids) }
   scope :since, ->(date) { where("updated_at > ?", date) }
 end
