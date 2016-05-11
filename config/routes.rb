cabc9b420a6d1b79461c82140aeb2be54d52c306
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-05-11 08:41:07 +0200

    linkcodetodevice from post to get

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get 'messages', to: 'api#messages'
   get 'getfullnamebycode/:code', to: 'api#get_fullname_by_code'
-  post 'linkcodetodevice', to: 'api#link_code_to_device'
+  get 'linkcodetodevice', to: 'api#link_code_to_device'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
