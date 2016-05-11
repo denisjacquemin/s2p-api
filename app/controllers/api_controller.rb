6bb20712285587acaf768f2af1ce5636e2daa650
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-05-11 08:59:52 +0200

    save new code to device

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -38,6 +38,7 @@ def link_code_to_device
     puts params.inspect
     device = Device.find_or_create_by(token: params[:token])
     device.codes << params[:code]
+    device.save
     render nothing: true
   end
 
