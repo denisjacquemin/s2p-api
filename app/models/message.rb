439abc9275876a705f06acc5dca5cdd57eaddd1a
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-06-19 15:26:19 +0200

    add getter and setter for signature

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,10 +1,12 @@
 class Message < ApplicationRecord
   has_many :mfiles
 
-  attr_accessor :students
+  attr_accessor :students, :signature
   def attributes
     super.merge('students' => self.students)
+    super.merge('signature' => self.signature)
   end
+
 
   scope :published, -> { where.not(publish_date: nil) }
   scope :by_group_ids, ->(group_ids) { where("groups && ARRAY[?]::integer[]", group_ids) }
