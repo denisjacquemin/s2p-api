99778d7d384eae048a23c7e806e89686293f24d7
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-06-26 09:36:16 +0200

    improve group name linked to a message

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -23,7 +23,7 @@ class ApiController < ApplicationController
         s.firstname if (!(s.groups & m.groups).empty?)
       }
       list_of_groups = groups.collect { |g|
-        g.name
+        g.name unless (m.groups.include? g.id)
       }
 
 
