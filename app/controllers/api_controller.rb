51272eae3c48fb0df8eeb8df4df0daf69d4ff6e7
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-06-26 11:32:50 +0200

    downcase codes submitted

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,7 +3,7 @@ class ApiController < ApplicationController
   def messages
     # receive codes corresponding to a student
     #codes = params[:codes].map{|code| {code: code[:code], latest_update: code[:latest_update]}}
-    codes = params[:codes]
+    codes = params[:codes].map {|c| c.downcase}
     student_codes = codes.select {|code| code.start_with?('s')}
     group_codes = codes.select {|code| code.start_with?('g')}
 
