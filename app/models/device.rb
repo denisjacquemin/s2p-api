15d290c994ffed9d437368041029bd6bae6569f8
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-07-16 18:10:07 +0200

    fix update postgres array

--- app/models/device.rb
+++ app/models/device.rb
@@ -9,11 +9,11 @@ class Device < ApplicationRecord
   # http://www.postgresql.org/docs/current/static/functions-array.html
   def add_code(code)
     #self.update_all(['codes = array_append(codes, ?)', code])
-    self.update(['codes = array_append(codes, ?)', code])
+    Device.where(uuid: self.uuid).update_all(['codes = array_append(codes, ?)', code])
   end
 
   def remove_code(code)
-    self.update(['codes = array_remove(codes, ?)', code])
+    Device.where(token: self.token).update_all(['codes = array_remove(codes, ?)', code])
   end
 
   def disable
