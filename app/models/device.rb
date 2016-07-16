f400088547b5349b08c0d00e5d35b46a922c5f60
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-07-16 17:51:18 +0200

    fix update_all -> update

--- app/models/device.rb
+++ app/models/device.rb
@@ -9,11 +9,11 @@ class Device < ApplicationRecord
   # http://www.postgresql.org/docs/current/static/functions-array.html
   def add_code(code)
     #self.update_all(['codes = array_append(codes, ?)', code])
-    self.update_all(['codes = array_append(codes, ?)', code])
+    self.update(['codes = array_append(codes, ?)', code])
   end
 
   def remove_code(code)
-    self.update_all(['codes = array_remove(codes, ?)', code])
+    self.update(['codes = array_remove(codes, ?)', code])
   end
 
   def disable
