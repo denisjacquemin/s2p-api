ebb5e16fae35c0de4c7e90e1465efdc437bbeb51
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-08-17 16:12:10 +0200

    get only active user

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -58,13 +58,13 @@ class ApiController < ApplicationController
   end
 
   def get_fullname_by_code
-    name = 'nothing found'
+    name = 'Code erroné'
     code = params[:code].downcase
     if code.start_with?("s")
       s = Student.by_codes(code).first
       name = s.fullname unless s.nil?
     elsif code.start_with?("g")
-      g = Group.by_codes(code).first
+      g = Group.by_codes(code).active.first
       name = g.name unless g.nil?
     elsif code.start_with?("u")
       u = User.by_codes(code).first
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
 
   scope :by_codes, ->(codes) { where(code: codes) }
+  scope :active, -> { where(deleted_at: nil) }
+
 
   def fullname
     "#{self.firstname} #{self.lastname}"
