256d07da612457d508824d3e3240048849126012
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-10-07 16:13:38 +0200

    update followers after_destroy

--- app/models/device.rb
+++ app/models/device.rb
@@ -3,6 +3,7 @@ class Device < ApplicationRecord
   validates :uuid, uniqueness: true
 
   after_save :update_followers, if: "codes_changed?"
+  after_destroy :update_followers
 
   enum notification_platform: [:ios, :android]
 
