7a01eaba39efc3f9d000aead6b7b4693c0e951c1
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-10-07 15:29:02 +0200

    more logs

--- app/jobs/update_followers.rb
+++ app/jobs/update_followers.rb
@@ -6,6 +6,8 @@ class UpdateFollowersJob < ApplicationJob
     students_ids_to_decrement = old_device_codes - new_device_codes
     students_ids_to_increment = new_device_codes - old_device_codes
 
+    logger.debug "students_ids_to_decrement: #{students_ids_to_decrement}"
+    logger.debug "students_ids_to_increment: #{students_ids_to_increment}"
     Student.by_code(students_ids_to_decrement).select(:id).each do |student|
       logger.debug "decrement #{student.followers} for student #{student.inspect}"
       student.decrement(:followers)
