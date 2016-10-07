ccf919cc802f67f2cac7519fd4c6540844c7a702
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-10-07 15:31:13 +0200

    removing select

--- app/jobs/update_followers.rb
+++ app/jobs/update_followers.rb
@@ -8,13 +8,13 @@ class UpdateFollowersJob < ApplicationJob
 
     logger.debug "students_ids_to_decrement: #{students_ids_to_decrement}"
     logger.debug "students_ids_to_increment: #{students_ids_to_increment}"
-    Student.by_code(students_ids_to_decrement).select(:id).each do |student|
+    Student.by_code(students_ids_to_decrement).each do |student|
       logger.debug "decrement #{student.followers} for student #{student.inspect}"
       student.decrement(:followers)
       student.save
     end
 
-    Student.by_code(students_ids_to_increment).select(:id).each do |student|
+    Student.by_code(students_ids_to_increment).each do |student|
       logger.debug "increment #{student.followers} for student #{student.inspect}"
       student.increment(:followers)
       student.save
