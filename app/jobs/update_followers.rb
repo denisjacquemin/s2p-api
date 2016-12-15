0bc6973d32362c99aea584ee57f283163a22e596
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-12-15 11:15:38 +0100

    followers algo fix

--- app/jobs/update_followers.rb
+++ app/jobs/update_followers.rb
@@ -3,24 +3,34 @@ class UpdateFollowersJob < ApplicationJob
 
   def perform(old_device_codes, new_device_codes)
     logger.debug "execute UpdateFollowersJob.perform(#{old_device_codes}, #{new_device_codes})"
-    students_ids_to_decrement = old_device_codes - new_device_codes
-    students_ids_to_increment = new_device_codes - old_device_codes
+    # collect all codes on which followers counter should be refreshed
+    codes_to_refresh = (old_device_codes + new_device_codes).uniq
+    students_to_refresh = Student.by_codes(codes_to_refresh)
 
-    logger.debug "students_ids_to_decrement: #{students_ids_to_decrement}"
-    logger.debug "students_ids_to_increment: #{students_ids_to_increment}"
-    Student.by_code(students_ids_to_decrement).each do |student|
-      logger.debug "decrement #{student.followers} for student #{student.inspect}"
-      if (student.followers > 0)
-        student.decrement(:followers)
-        student.save
-      end
+    students_to_refresh.each do |student|
+      # check the number of device dollowing the student
+      followers_count = Device.by_code(student.code)
+      student.update(followers: followers_count)
     end
 
-    Student.by_code(students_ids_to_increment).each do |student|
-      logger.debug "increment #{student.followers} for student #{student.inspect}"
-      student.increment(:followers)
-      student.save
-    end
+    # students_ids_to_decrement = old_device_codes - new_device_codes
+    # students_ids_to_increment = new_device_codes - old_device_codes
+    #
+    # #logger.debug "students_ids_to_decrement: #{students_ids_to_decrement}"
+    # #logger.debug "students_ids_to_increment: #{students_ids_to_increment}"
+    # Student.by_code(students_ids_to_decrement).each do |student|
+    #   logger.debug "decrement #{student.followers} for student #{student.inspect}"
+    #   if (student.followers > 0)
+    #     student.decrement(:followers)
+    #     student.save
+    #   end
+    # end
+    #
+    # Student.by_code(students_ids_to_increment).each do |student|
+    #   logger.debug "increment #{student.followers} for student #{student.inspect}"
+    #   student.increment(:followers)
+    #   student.save
+    # end
 
   end
 end
