87ae85e5b687ebba96354350fa52c70a39969477
Denis Jacquemin <denis.jacquemin@gmail.com>
2017-05-06 14:12:15 +0200

    removing scout

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,4 @@
 class ApiController < ApplicationController
-  include ScoutApm::Instruments::ActionControllerRails3Rails4Instruments
 
   def messages
     # receive codes corresponding to a student
