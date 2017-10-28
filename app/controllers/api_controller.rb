c232130d77ea8303d877923418b391ceddf35d03
Denis Jacquemin <denis.jacquemin@gmail.com>
2017-10-28 09:31:14 +0200

    testing setInterval

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -43,7 +43,7 @@ class ApiController < ApplicationController
           m.content << "<div style='background: #ff4785; padding: 40px 0; width: 100%; text-align: center; margin: 0; height: 100px; line-height: 100px;'>"\
               "<a style='padding: 15px 10px; background: #00cb75; color:#fff;' href='thetransaction'>Payer avec payconiq</a>"\
             "</div>"\
-            "<script>alert('Coucou')</script>"
+            "<script>setInterval(function(){ console.log('setInterval done') }, 3000);</script>"
         end
 
 
