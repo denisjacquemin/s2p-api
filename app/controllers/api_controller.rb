2834ec7d8074ff06c7b96b5b7a524898edcb0ae2
Denis Jacquemin <denis.jacquemin@gmail.com>
2017-11-01 12:11:07 +0100

    improving button layout\

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -41,8 +41,8 @@ class ApiController < ApplicationController
         # if payment is required append a payconiq button
         if m.amount_to_pay > 0
           konectoapp_host = ENV["KONECTOAPP_HOST"]
-          m.content << "<div style='background: #ff4785; padding: 20px 0; width: 100%; text-align: center; margin: 0;'>"\
-              "<a style='padding: 15px 10px; background: #00cb75; color:#fff;' href='https://#{konectoapp_host}/p/#{m.muuid}#paysection'>Payer #{m.amount_to_pay}&euro;</a>"\
+          m.content << "<div style='background: #ff4785; padding: 40px 0; width: 100%; text-align: center; margin: 0;'>"\
+              "<a style='padding: 15px 50px; background: #00cb75; color:#fff;' href='https://#{konectoapp_host}/p/#{m.muuid}#paysection'>Payer #{m.amount_to_pay}&euro;</a>"\
             "</div>"
         end
 
