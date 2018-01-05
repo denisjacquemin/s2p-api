b6be7fc4217c4aa83982abb53fe9fcbd91152d37
Denis Jacquemin <denis.jacquemin@gmail.com>
2018-01-05 14:42:32 +0100

    back with old button style

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -94,10 +94,9 @@ class ApiController < ApplicationController
 
           if m.school.payconiq_enable?
             konectoapp_host = ENV["KONECTOAPP_HOST"]
-            # m.content << "<div style='background: #ff4785; padding: 40px 0; width: 100%; text-align: center; margin: 0;'>"\
-            #     "<a style='padding: 15px 50px; background: #00cb75; color:#fff;' href='https://#{konectoapp_host}/p/#{m.muuid}?s=#{list_of_students_firstname_and_lastname.compact.join(', ')}#paysection'>Payer #{m.amount_to_pay}&euro;</a>"\
-            #   "</div>"
-            m.content << "<br><br><a style='padding: 25px 50px; background: #ff4785; color:#fff;' href='https://#{konectoapp_host}/p/#{m.muuid}?s=#{list_of_students_firstname_and_lastname.compact.join(', ')}#paysection'>Payer #{m.amount_to_pay}&euro;</a>"
+            m.content << "<div style='padding: 40px 0; width: 100%; text-align: center; margin: 0;'>"\
+                "<a style='padding: 25px 50px; background: #ff4785; color:#fff;' href='https://#{konectoapp_host}/p/#{m.muuid}?s=#{list_of_students_firstname_and_lastname.compact.join(', ')}#paysection'>Payer #{m.amount_to_pay}&euro;</a>"\
+              "</div>"
           end
         end
 
