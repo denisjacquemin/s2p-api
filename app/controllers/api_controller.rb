0ddba03f1d6c15e7cb0d690d01ce4961071ad47c
Denis Jacquemin <denis.jacquemin@gmail.com>
2018-06-04 09:49:49 +0200

    remove forms association

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -17,7 +17,7 @@ class ApiController < ApplicationController
       duuid = params[:uuid]
       device = Device.find_by_uuid(duuid)
       # find messages based on the groups found
-      @messages =   Message.published.includes(:photo_files, :forms).for_app.by_group_and_student_ids(groups_ids, student_ids).order(updated_at: :desc).limit(30) #.includes(:mfiles)
+      @messages =   Message.published.includes(:photo_files).for_app.by_group_and_student_ids(groups_ids, student_ids).order(updated_at: :desc).limit(30) #.includes(:mfiles)
       students = Student.by_codes(student_codes)
       groups = Group.by_codes(group_codes)
 
