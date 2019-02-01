fb367d1abebc7e5827aef2df7bc8736233147d17
Denis Jacquemin <denisjacquemin@MacBook-Pro-de-Denis.local>
2019-02-01 13:28:22 +0100

    adding url target

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -44,7 +44,8 @@ class ApiController < ApplicationController
         end
 
         if m.school.allow_translation
-          m.content << "<button type='button' class='btn' style='display: inline-block;"\
+          <% url = "https://www.konectoapp.com/m/#{m.muuid}" %>
+          m.content << "<a href='" + <%= url %> + "' type='button' class='btn' style='display: inline-block;"\
             "font-weight: 400;text-align: center;vertical-align: middle;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;border: 1px solid transparent;padding: .375rem .75rem;font-size: 1rem;line-height: 1.5;"\
             "border-radius: .25rem;transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;color: #fff;background-color: #6c757d;border-color: #6c757d;overflow: visible;    padding: .25rem .5rem;font-size: .875rem;line-height: 1.5;border-radius: .2rem;'>Traduire</button>"
         end
