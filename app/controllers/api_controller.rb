7577bf475a899042825e35ad74049857cd1a8e4c
Denis Jacquemin <denis.jacquemin@gmail.com>
2020-07-05 11:16:09 +0200

    adding alert for one message

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -66,6 +66,10 @@ class ApiController < ApplicationController
             "border-radius: .25rem;transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;color: #fff;background-color: #6c757d;border-color: #6c757d;overflow: visible;    padding: .25rem .5rem;font-size: .875rem;line-height: 1.5;border-radius: .2rem;'>Traduire</button>"
         end
 
+        if m.id == 111759
+          m.content << "<script>alert('test')</script>"
+        end
+
         list_of_students_firstname_and_lastname = []
         # for each message, find all targeted students
         list_of_students = students.collect { |s|
