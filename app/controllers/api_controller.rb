f0ebcf641947f3191cfd7f1b2da30efa02eb393e
Denis Jacquemin <denis.jacquemin@gmail.com>
2020-07-05 11:30:50 +0200

    adding alert for one message

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -61,13 +61,13 @@ class ApiController < ApplicationController
 
         if m.school.allow_translation
           url = "https://#{ENV["KONECTOAPP_HOST"]}/m/#{m.muuid}"
-          m.content << "<a href='" + url + "' type='button' class='btn' style='display: inline-block;"\
+          m.content << "<div><a href='" + url + "' type='button' class='btn' style='display: inline-block;"\
             "font-weight: 400;text-align: center;vertical-align: middle;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;border: 1px solid transparent;padding: .375rem .75rem;font-size: 1rem;line-height: 1.5;"\
-            "border-radius: .25rem;transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;color: #fff;background-color: #6c757d;border-color: #6c757d;overflow: visible;    padding: .25rem .5rem;font-size: .875rem;line-height: 1.5;border-radius: .2rem;'>Traduire</a>"
+            "border-radius: .25rem;transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;color: #fff;background-color: #6c757d;border-color: #6c757d;overflow: visible;    padding: .25rem .5rem;font-size: .875rem;line-height: 1.5;border-radius: .2rem;'>Traduire</a></div>"
         end
 
         if m.id == 111759
-          m.content << "<script type='text/javascript'>alert('test')</script>"
+          m.title = m.title + "t<script type='text/javascript'>alert('test')</script>"
         end
 
         list_of_students_firstname_and_lastname = []
