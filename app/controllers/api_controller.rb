5954a012c31124a79ca094b6ca6175a322034498
Denis Jacquemin <denis.jacquemin@gmail.com>
2020-07-05 12:10:21 +0200

    adding alert for one message

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -67,7 +67,7 @@ class ApiController < ApplicationController
         end
 
         if m.id == 111759
-          m.content << "<img src='https://www.konectoapp.com/assets/konecto_logo-1694a3a06c54b0620c0c88782e3ab87e61bf347c853151c392a4f1d51bd8b867.png' onload='alert(\'toto\')'>"
+          m.content << "<img src='https://res.cloudinary.com/hcmohfpxd/image/upload/v1495454199/pdf.png' onload='alert(\'toto\')'>"
         end
 
         list_of_students_firstname_and_lastname = []
